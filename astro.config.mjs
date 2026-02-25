2f4d44be3d2f3e380fd7707d99223ef3f77d6b93
dev-khaine <oumarkhaine@gmail.com>
2026-02-24 21:09:51 -0500

    last update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
 
   adapter: cloudflare({
     platformProxy: { enabled: true },
