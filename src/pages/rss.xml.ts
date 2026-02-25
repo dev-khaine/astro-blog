470c827dbb70f3fc1d80a43ea3cb22319b2b66d6
dev-khaine <oumarkhaine@gmail.com>
2026-02-24 21:19:27 -0500

    last update

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-export async function GET(context: { site: URL }) {
+export async function GET(context) {
   const posts = (await getCollection("blog", p => !p.data.draft))
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
